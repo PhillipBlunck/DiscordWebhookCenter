72c1b70a2668a78fb4c50cf2e5121f6724d0f9dd
Phillip Blunck <phillip.blunck@outlook.de>
2020-10-06 21:31:28 +0200

    Main module uses now object oriented GUI

--- DiscordWebhookCenter.py
+++ DiscordWebhookCenter.py
@@ -3,18 +3,25 @@
 ######################################################################################
 ##                                                                                  ##
 ## Discord Webhook Center                                                           ##
+##                                                                                  ##
 ## Phillip Blunck, 2020-09-27                                                       ##
 ##                                                                                  ##
 ######################################################################################
 
 import sys
 import tkinter as tk
+import gateway as gate
+
+######################################################################################
+
+DEBUG_FLAG = True
+REQUEST_SUCCESS = 204
 
 ######################################################################################
 
 class MainApp:
 
-    def __init__(self, master):
+    def __init__(self, master, gateway):
 
         # Create main frame for the application
         frame = tk.Frame(master, bg="lightblue")
@@ -48,7 +55,7 @@ class MainApp:
         self.lbl_descrpt = tk.Label(master=self.frm_content,
             text="Description:", padx=20
         )
-        self.entr_descrpt = tk.Entry(master=self.frm_content, width=80)
+        self.txt_descrpt = tk.Text(master=self.frm_content, width=60, height=28)
         self.lbl_fields = tk.Label(master=self.frm_content, text="Fields:", padx=20)
         self.entr_fields = tk.Entry(master=self.frm_content, width=80)
         self.lbl_picture = tk.Label(master=self.frm_content, text="Picture:", padx=20)
@@ -60,8 +67,8 @@ class MainApp:
         self.entr_author.grid(row=0, column=1, sticky="w")
         self.lbl_title.grid(row=1, column=0, sticky="w")
         self.entr_title.grid(row=1, column=1, sticky="w")
-        self.lbl_descrpt.grid(row=2, column=0, sticky="w")
-        self.entr_descrpt.grid(row=2, column=1, sticky="w")
+        self.lbl_descrpt.grid(row=2, column=0, sticky="nw")
+        self.txt_descrpt.grid(row=2, column=1, sticky="w")
         self.lbl_fields.grid(row=3, column=0, sticky="w")
         self.entr_fields.grid(row=3, column=1, sticky="w")
         self.lbl_picture.grid(row=4, column=0, sticky="w")
@@ -77,14 +84,16 @@ class MainApp:
         )
         self.txt_return = tk.Text(master=self.frm_result)
         self.txt_return.config(state=tk.NORMAL)
-        self.txt_return.insert(tk.INSERT,"Return status of webhook message.")
+        self.txt_return.insert(tk.INSERT,"Return status of webhook message.\n")
         self.txt_return.config(state=tk.DISABLED)
 
         self.btn_settings = tk.Button(master=self.frm_result,
-            text="Settings", bg="lightyellow", pady=10, command=self.settings_btn_exec
+            text="Settings", bg="lightyellow", pady=10,
+            command=self.settings_btn_exec
         )
         self.btn_send = tk.Button(master=self.frm_result,
-            text="Send", bg="lightblue", pady=10, command=self.send_btn_exec
+            text="Send", bg="lightblue", pady=10,
+            command=self.send_btn_exec
         )
         self.lbl_result.pack(side=tk.TOP)
         self.txt_return.pack(fill=tk.BOTH, side=tk.TOP, expand=1)
@@ -96,11 +105,40 @@ class MainApp:
         self.frm_content.pack(fill=tk.BOTH, side=tk.LEFT, expand=1)
         self.frm_result.pack(fill=tk.BOTH, side=tk.LEFT, expand=1)
 
+        # gateway for client server handling
+        self.gateway = gateway
+
     def send_btn_exec(self):
-        print("Sending data!")
+        """When a user pressed the send button, the data of all input fields
+        is send to a discord server by the gateway.
+        """
+        data = []
+        data.append(self.entr_title.get())
+        data.append(self.txt_descrpt.get(0.0, tk.END))
+        data.append(self.entr_author.get())
+        data.append(self.entr_picture.get())
+        data.append(self.entr_footer.get())
+        # send data
+        msg = self.gateway.send(data)
+        # See https://discord.com/developers/docs/topics/opcodes-and-status-codes
+        # for status codes
+        if msg <= REQUEST_SUCCESS: # TODO: Maybe exception handling better
+            msg = "Message successfully\nsend."
+        else:
+            msg = f"Failed to send message.\nError code: {msg}"
+        # show response in history
+        self.txt_return.config(state=tk.NORMAL)
+        self.txt_return.insert(tk.INSERT, msg)
+        self.txt_return.config(state=tk.DISABLED)
+        # clear all inputs
+        self.clearInputs()
 
     def settings_btn_exec(self):
-        print("Open settings!")
+        if DEBUG_FLAG: print("Open settings!")
+
+    def clearInputs(self):
+        # TODO: Clear the Entry-Fields
+        pass
 
 ######################################################################################
 
@@ -108,18 +146,20 @@ def main():
     """This function starts the Discord Webhook Center application.
     It generates a new window, where the user can send content to a discord server.
     """
-
     # create window
     root = tk.Tk()
     root.title("Discord Webhook Center")
     root.resizable(width=False, height=False)
     root.geometry("800x600")
-
-    app = MainApp(root)
+    # set gateway instance
+    gateway = gate.Gateway()
+    # load gui
+    app = MainApp(root, gateway)
+    app.clearInputs()
     # Run mainloop
     root.mainloop()
     # destroy explicit (used for other development environments)
-    root.destroy()
+    # root.destroy()
 
 ######################################################################################
 
