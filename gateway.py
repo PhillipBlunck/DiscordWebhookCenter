70cbcf243320dc2c3b2983f8a3011ef5ff659501
Phillip Blunck <phillip.blunck@outlook.de>
2020-10-08 23:04:53 +0200

    sending checks now data and limit of chars

--- gateway.py
+++ gateway.py
@@ -14,6 +14,12 @@ import file
 
 ######################################################################################
 
+EMPTY = ""
+CHAR_LIM = 500
+DEBUG_FLAG = True
+
+######################################################################################
+
 class Gateway:
 
     def __init__(self):
@@ -39,19 +45,35 @@ class Gateway:
         and returns the execution response.
         """
         # Collect data
-        # TODO: Check for character limitation !
-        embed = DiscordEmbed(
-            title=data[0],
-            description=data[1],
-            color=242424
-        )
-        embed.set_author = data[2]
-        embed.set_image(url=data[3])
-        embed.set_footer(text=data[4])
-        embed.set_timestamp()
-        self.webhook.add_embed(embed)
-        # send data and get response
-        response = self.webhook.execute()
-        return response.status_code
+        if self.webhook.url != EMPTY and not self.check_data_limit(data):
+            embed = DiscordEmbed(
+                title=data[0],
+                description=data[1],
+                color=242424
+            )
+            embed.set_author = data[2]
+            embed.set_image(url=data[3])
+            embed.set_footer(text=data[4])
+            embed.set_timestamp()
+            self.webhook.add_embed(embed)
+            # send data and get response
+            response = self.webhook.execute()
+            ret = response.status_code
+        else:
+            ret = 1337
+        return ret
 
+    def check_data_limit(self, data):
+        """Checks if the characters of the data is in set bounds.
+        Returns True when one input is out of bounds.
+        Returns False when each input is in bounds.
+        """
+        ret = False
+        for dat in data:
+            if DEBUG_FLAG: print(f"Len of DATA o: {len(dat)}")
+            if len(dat) > CHAR_LIM or len(dat) == 0:
+                ret = True
+                break
+        return ret
+        
 ######################################################################################
