9ac2bf420af68a034cac3d699d4be09c0df3a158
Phillip Blunck <phillip.blunck@outlook.de>
2020-10-09 14:46:32 +0200

    Added reading method for json file and added debug for standalone mode

--- file.py
+++ file.py
@@ -9,9 +9,11 @@
 ######################################################################################
 
 import sys
+import json
 
 ######################################################################################
 
+DEBUG_FLAG = True
 EMPTY = ""
 
 ######################################################################################
@@ -22,12 +24,25 @@ class FileHandler:
         self.filename = file
 
     def read(self):
-        pass
+        """Opens the json file and returns a list with the settings.
+        If no file is found it returns an error message.
+        """
+        retdata = []
+        try:
+            with open(self.filename) as json_file:
+                data = json.load(json_file)
+                retdata.append(data["server1"])
+                retdata.append(data["webhook1"])
+                retdata.append(data["server2"])
+                retdata.append(data["webhook2"])
+        except FileNotFoundError:
+            retdata.append("No json file found!")
+        return retdata
 
     def write(self):
         pass
 
-    def create(self):
-        pass
-
 ######################################################################################
+
+if __name__ == "__main__":
+    if DEBUG_FLAG: hdl = FileHandler("settings.json"); test = hdl.read(); print(test)
